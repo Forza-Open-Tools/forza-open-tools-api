69cd80017c0c999c8e04163887ee3e942701eb19
turbodoubiez <romberg118+github_alt@gmail.com>
2022-02-16 14:01:15 +0100

    Add named selects, use camelCase

--- src/routes/makes/Controller.ts
+++ src/routes/makes/Controller.ts
@@ -4,7 +4,14 @@ import { NotFoundError, Make } from '../../lib/types';
 
 export default class MakesController {
     async make(makeId: number): Promise<Make> {
-        const make: Make = await knex<Make>(tbls.makes).first('*').where('id', makeId);
+        const make: Make = await knex<Make>(tbls.makes)
+            .first(
+                'id',
+                'display_name AS displayName',
+                'manufacturer_code AS manufacturerCode',
+                'country_id AS countryId'
+            )
+            .where('id', makeId);
 
         if (!make) {
             throw new NotFoundError(`Make #${makeId} does not exist.`);
@@ -14,7 +21,12 @@ export default class MakesController {
     }
 
     async makes(): Promise<Make[]> {
-        const makes: Make[] = await knex<Make>(tbls.makes).select('*');
+        const makes: Make[] = await knex<Make>(tbls.makes).select(
+            'id',
+            'display_name AS displayName',
+            'manufacturer_code AS manufacturerCode',
+            'country_id AS countryId'
+        );
 
         return makes;
     }
